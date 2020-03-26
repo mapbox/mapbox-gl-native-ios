maplibre/maplibre-native

commit 8b12c6b305d1f03faac9c71dc696b406ab55e309

Add async completion handler to MGLOfflineStorage::preloadData (#188)
* Add async completion handler to preloadData

* Update unit tests

* Update changelogs for iOS and macOS

platform/darwin/src/MGLOfflineStorage.mm
@@ -80,7 +80,9 @@ const MGLExceptionName MGLUnsupportedRegionTypeException = @"MGLUnsupportedRegio
     if (self.isPaused) {
         return;
     }
-    _mbglFileSource->pause();
+
+    _mbglOnlineFileSource->pause();
+    _mbglDatabaseFileSource->pause();
     self.paused = YES;
 }
 
@@ -88,7 +90,9 @@ const MGLExceptionName MGLUnsupportedRegionTypeException = @"MGLUnsupportedRegio
     if (!self.isPaused) {
         return;
     }
-    _mbglFileSource->resume();
+
+    _mbglOnlineFileSource->pause();
+    _mbglDatabaseFileSource->pause();
     self.paused = NO;
 }
 #endif
@@ -281,9 +285,9 @@ const MGLExceptionName MGLUnsupportedRegionTypeException = @"MGLUnsupportedRegio
     } else if ([keyPath isEqualToString:@"apiBaseURL"] && object == [MGLAccountManager sharedManager]) {
         NSURL *apiBaseURL = change[NSKeyValueChangeNewKey];
         if ([apiBaseURL isKindOfClass:[NSNull class]]) {
-            _mbglFileSource->setProperty(mbgl::API_BASE_URL_KEY, mbgl::util::API_BASE_URL);
+            _mbglOnlineFileSource->setProperty(mbgl::API_BASE_URL_KEY, mbgl::util::API_BASE_URL);
         } else {
-            _mbglFileSource->setProperty(mbgl::API_BASE_URL_KEY, apiBaseURL.absoluteString.UTF8String);
+            _mbglOnlineFileSource->setProperty(mbgl::API_BASE_URL_KEY, apiBaseURL.absoluteString.UTF8String);
         }
     } else {
         [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
@@ -633,6 +637,11 @@ const MGLExceptionName MGLUnsupportedRegionTypeException = @"MGLUnsupportedRegio
 }
 
 - (void)preloadData:(NSData *)data forURL:(NSURL *)url modificationDate:(nullable NSDate *)modified expirationDate:(nullable NSDate *)expires eTag:(nullable NSString *)eTag mustRevalidate:(BOOL)mustRevalidate {
+    [self preloadData:data forURL:url modificationDate:modified expirationDate:expires eTag:eTag mustRevalidate:mustRevalidate completionHandler:nil];
+}
+
+- (void)preloadData:(NSData *)data forURL:(NSURL *)url modificationDate:(nullable NSDate *)modified expirationDate:(nullable NSDate *)expires eTag:(nullable NSString *)eTag mustRevalidate:(BOOL)mustRevalidate
+    completionHandler:(nullable MGLOfflinePreloadDataCompletionHandler)completion {
     mbgl::Resource resource(mbgl::Resource::Kind::Unknown, url.absoluteString.UTF8String);
     mbgl::Response response;
     response.data = std::make_shared<std::string>(static_cast<const char*>(data.bytes), data.length);
@@ -649,8 +658,16 @@ const MGLExceptionName MGLUnsupportedRegionTypeException = @"MGLUnsupportedRegio
     if (expires) {
         response.expires = mbgl::Timestamp() + std::chrono::duration_cast<mbgl::Seconds>(MGLDurationFromTimeInterval(expires.timeIntervalSince1970));
     }
-    
-    _mbglDatabaseFileSource->put(resource, response);
+
+    std::function<void()> callback;
+
+    if (completion) {
+        callback = [completion, url] {
+            dispatch_async(dispatch_get_main_queue(), [completion, url] { completion(url, nil); });
+        };
+    }
+
+    _mbglDatabaseFileSource->forward(resource, response, callback);
 }
 
 - (void)putResourceWithUrl:(NSURL *)url data:(NSData *)data modified:(nullable NSDate *)modified expires:(nullable NSDate *)expires etag:(nullable NSString *)etag mustRevalidate:(BOOL)mustRevalidate {

platform/darwin/src/MGLOfflineStorage.h
@@ -143,6 +143,15 @@ typedef void (^MGLOfflinePackRemovalCompletionHandler)(NSError * _Nullable error
 typedef void (^MGLBatchedOfflinePackAdditionCompletionHandler)(NSURL *fileURL, NSArray<MGLOfflinePack *> * _Nullable packs, NSError * _Nullable error);
 
 /**
+A block to be called once the data  has been preloaded.
+
+ @param url The URL of the data that was pre-loaded.
+ @param error Contains a pointer to an error object (if any) indicating why the
+ data could not be pre-loaded.
+*/
+typedef void (^MGLOfflinePreloadDataCompletionHandler)(NSURL * url, NSError * _Nullable error);
+
+/**
  The type of resource that is requested.
  */
 typedef NS_ENUM(NSUInteger, MGLResourceKind) {
@@ -434,7 +443,9 @@ MGL_EXPORT
  This method is asynchronous; the data may not be immediately available for
  in-progress requests, though subsequent requests should have access to the
  cached data.
- 
+
+ To find out when the resource is ready to retrieve from the cache, use the -preloadData:forURL:modificationDate:expirationDate:eTag:mustRevalidate:completionHandler: method.
+
  @param data Response data to store for this resource. The data is expected to
     be uncompressed; internally, the cache will compress data as necessary.
  @param url The URL at which the data can normally be found.
@@ -447,6 +458,30 @@ MGL_EXPORT
 - (void)preloadData:(NSData *)data forURL:(NSURL *)url modificationDate:(nullable NSDate *)modified expirationDate:(nullable NSDate *)expires eTag:(nullable NSString *)eTag mustRevalidate:(BOOL)mustRevalidate NS_SWIFT_NAME(preload(_:for:modifiedOn:expiresOn:eTag:mustRevalidate:));
 
 - (void)putResourceWithUrl:(NSURL *)url data:(NSData *)data modified:(nullable NSDate *)modified expires:(nullable NSDate *)expires etag:(nullable NSString *)etag mustRevalidate:(BOOL)mustRevalidate __attribute__((deprecated("", "-preloadData:forURL:modificationDate:expirationDate:eTag:mustRevalidate:")));
+
+/**
+Inserts the provided resource into the ambient cache, calling a completion handler when finished.
+
+This method is asynchronous. The data is available for in-progress requests as
+soon as the completion handler is called.
+
+This method is asynchronous; the data may not be immediately available for
+in-progress requests, though subsequent requests should have access to the
+cached data.
+
+@param data Response data to store for this resource. The data is expected to
+   be uncompressed; internally, the cache will compress data as necessary.
+@param url The URL at which the data can normally be found.
+@param modified The date the resource was last modified.
+@param expires The date after which the resource is no longer valid.
+@param eTag An HTTP entity tag.
+@param mustRevalidate A Boolean value indicating whether the data is still
+   usable past the expiration date.
+@param completion The completion handler to call once the data has been preloaded.
+This handler is executed asynchronously on the main queue.
+*/
+- (void)preloadData:(NSData *)data forURL:(NSURL *)url modificationDate:(nullable NSDate *)modified expirationDate:(nullable NSDate *)expires eTag:(nullable NSString *)eTag mustRevalidate:(BOOL)mustRevalidate
+    completionHandler:(nullable MGLOfflinePreloadDataCompletionHandler)completion;
 
 @end
 

mapbox/mapbox-gl-native-ios

commit 49011ec1b11856efe1e09bac0a7da137be649209

Resume thread in MGLOfflineStorage::unpauseFileSource
Fixes pause/resume bug introduced in https://github.com/mapbox/mapbox-gl-native-ios/pull/188

platform/darwin/src/MGLOfflineStorage.mm
@@ -91,8 +91,8 @@ const MGLExceptionName MGLUnsupportedRegionTypeException = @"MGLUnsupportedRegio
         return;
     }
 
-    _mbglOnlineFileSource->pause();
-    _mbglDatabaseFileSource->pause();
+    _mbglOnlineFileSource->resume();
+    _mbglDatabaseFileSource->resume();
     self.paused = NO;
 }
 #endif