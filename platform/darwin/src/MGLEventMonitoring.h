77eb6999d92ff4e73799f08b798a067a18eaa620
jmkiley <jordan.kiley@mapbox.com>
2020-07-23 13:31:41 -0700

    Add MGLEventMonitoring

---
+++ platform/darwin/src/MGLEventMonitoring.h
@@ -0,0 +1,37 @@
+#import "MGLMapView+Impl.h"
+#import "MGLFoundation.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+
+// TODO: DOCS, this might not need to be public
+typedef NSString *MGLResourceType NS_TYPED_ENUM;
+FOUNDATION_EXPORT MGL_EXPORT MGLResourceType const MGLResourceTypeUnknown;
+FOUNDATION_EXPORT MGL_EXPORT MGLResourceType const    MGLResourceTypeStyle;
+FOUNDATION_EXPORT MGL_EXPORT MGLResourceType const    MGLResourceTypeSource;
+FOUNDATION_EXPORT MGL_EXPORT MGLResourceType const    MGLResourceTypeTile;
+FOUNDATION_EXPORT MGL_EXPORT MGLResourceType const    MGLResourceTypeGlyphs;
+FOUNDATION_EXPORT MGL_EXPORT MGLResourceType const    MGLResourceTypeSpriteImage;
+FOUNDATION_EXPORT MGL_EXPORT MGLResourceType const    MGLResourceTypeSpriteJSON;
+FOUNDATION_EXPORT MGL_EXPORT MGLResourceType const    MGLResourceTypeImage;
+
+@interface MGLEventMonitoringRequest : NSObject
+@property (nonatomic) NSURL *url;
+@property (nonatomic) MGLResourceType *resourceType;
+@property (nonatomic) NSString *priority; // This can have two values, regular or low. Should make enum?
+
+- (MGLEventMonitoringRequest *)requestWithURL:(NSURL *)url andResourceType:(MGLResourceType)resourceType withPriorityLevel
+@property
+@end
+
+@interface MGLEventMonitoringResponse : NSObject
+
+@end
+
+@interface MGLEventMonitoring : NSObject
+
+- (MGLEvemtMonitoringResponse *)getResponseFromMapView:(MGLMapView *)mapView for (MGLEventMonitoringRequest *)request;
+@end
+
+
+NS_ASSUME_NONNULL_END
---
+++ platform/darwin/src/MGLEventMonitoring.m
@@ -0,0 +1,16 @@
+
+#import <mbgl/map/map_observer.hpp>
+#import "MGLEventMonitoring.h"
+
+
+const MGLResourceTypeUnknown = @"unknown";
+const MGLResourceTypeStyle = @"style";
+const MGLResourceTypeSource = @"source";
+const MGLResourceTypeTile = @"tile";
+const MGLResourceTypeGlyphs = @"glyphs";
+const MGLResourceTypeSpriteImage = @"sprite-image";
+const MGLResourceTypeSpriteJSON = @"sprite-json";
+const MGLResourceTypeImage = @"image";
+@implementation MGLEventMonitoring
+
+@end
