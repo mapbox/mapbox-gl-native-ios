9150e0b4570eecdfea9fa6034864310f4224c97c
jmkiley <jordan.kiley@mapbox.com>
2020-07-27 23:51:32 -0700

    Switch to MGLObserver

--- platform/darwin/src/MGLObserver.h
+++ platform/darwin/src/MGLObserver.h
@@ -5,38 +5,20 @@ NS_ASSUME_NONNULL_BEGIN
 
 
 // TODO: DOCS, this might not need to be public
-typedef NSString *MGLResourceType NS_TYPED_ENUM;
-FOUNDATION_EXPORT MGL_EXPORT MGLResourceType const MGLResourceTypeUnknown;
-FOUNDATION_EXPORT MGL_EXPORT MGLResourceType const    MGLResourceTypeStyle;
-FOUNDATION_EXPORT MGL_EXPORT MGLResourceType const    MGLResourceTypeSource;
-FOUNDATION_EXPORT MGL_EXPORT MGLResourceType const    MGLResourceTypeTile;
-FOUNDATION_EXPORT MGL_EXPORT MGLResourceType const    MGLResourceTypeGlyphs;
-FOUNDATION_EXPORT MGL_EXPORT MGLResourceType const    MGLResourceTypeSpriteImage;
-FOUNDATION_EXPORT MGL_EXPORT MGLResourceType const    MGLResourceTypeSpriteJSON;
-FOUNDATION_EXPORT MGL_EXPORT MGLResourceType const    MGLResourceTypeImage;
-
-@interface MGLEventMonitoringRequest : NSObject
-@property (nonatomic) NSURL *url;
-@property (nonatomic) MGLResourceType resourceType;
-@property (nonatomic) NSString *priority; // This can have two values, regular or low. Should make enum?
-
-- (MGLEventMonitoringRequest *)requestWithURL:(NSURL *)url andResourceType:(MGLResourceType)resourceType withPriorityLevel:(NSString *)priorityLevel;
-@end
-
-@interface MGLEventMonitoringResponse : NSObject
-
-@end
+typedef NSString *MGLObserverEvent NS_TYPED_ENUM;
+FOUNDATION_EXPORT MGL_EXPORT MGLObserverEvent const MGLObserverEventResourceRequest;
 
 @interface MGLObserver : NSObject
 @property (nonatomic) NSUInteger identifier;
+- (void)subscribeToObserver:(MGLObserver *)observer forEventType:(NSString *)eventType;
+- (void)unsubscribeFromObserver:(MGLObserver *)observer;
 @end
 
 
 @protocol MGLObservable
 
-- (void)subscribeToObserver:(MGLObserver *)observer forEventType:(NSString *)eventType;
-- (void)unsubscribeFromObserver:(MGLObserver *)observer;
-//- (MGLEventMonitoringResponse *)getResponseFromMapView:(MGLMapView *)mapView for (MGLEventMonitoringRequest *)request;
+
+
 @end
 
 
--- platform/darwin/src/MGLObserver.mm
+++ platform/darwin/src/MGLObserver.mm
@@ -1,26 +1,37 @@
 
-#include <mbgl/util/observable.hpp>
+#import <mbgl/util/observable.hpp>
 
 #import "MGLObserver.h"
 
+// TODO: Should there be separate bindings for each observer (map, file source, etc)?
+const MGLObserverEvent MGLObserverEventResourceRequest = @"resource-request";
 
-//const MGLResourceTypeUnknown = @"unknown";
-//const MGLResourceTypeStyle = @"style";
-//const MGLResourceTypeSource = @"source";
-//const MGLResourceTypeTile = @"tile";
-//const MGLResourceTypeGlyphs = @"glyphs";
-//const MGLResourceTypeSpriteImage = @"sprite-image";
-//const MGLResourceTypeSpriteJSON = @"sprite-json";
-//const MGLResourceTypeImage = @"image";
+@interface MGLObserver()
+@property (nonatomic) std::shared_ptr<mbgl::Observable> mbglObservable;
+@property (nonatomic) std::shared_ptr<mbgl::Observer> mbglObserver;
+@end
+
 @implementation MGLObserver
 
-//- (instancetype)init
-//{
-//    self = [super init];
-//    if (self) {
-//        
-//    }
-//    return self;
-//}
+- (instancetype)initWithObservable:(std::shared_ptr<mbgl::Observer>)observer
+{
+    self = [super init];
+    if (self) {
+        _mbglObserver = observer;
+    }
+    return self;
+}
+
+- (void)subscribeToObserver:(MGLObserver *)observer forEventType:(MGLObserverEvent)eventType
+{
+
+    auto *cEventType = [eventType cStringUsingEncoding:NSUTF8StringEncoding];
+    _mbglObservable->subscribe(_mbglObserver, eventType);
+}
+
+- (void)unsubscribeFromObserver:(MGLObserver *)observer
+{
+
+}
 
 @end
