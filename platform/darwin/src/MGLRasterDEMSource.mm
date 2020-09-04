21bd7a3fb435633224b2915b05519f38e1a30b10
Julian Rex <julian.rex@gmail.com>
2020-09-03 22:43:11 -0400

    Explicit string argument

--- platform/darwin/src/MGLRasterDEMSource.mm
+++ platform/darwin/src/MGLRasterDEMSource.mm
@@ -8,7 +8,8 @@
 @implementation MGLRasterDEMSource
 
 - (std::unique_ptr<mbgl::style::RasterSource>)pendingSourceWithIdentifier:(NSString *)identifier urlOrTileset:(mbgl::variant<std::string, mbgl::Tileset>)urlOrTileset tileSize:(uint16_t)tileSize {
-    auto source = std::make_unique<mbgl::style::RasterDEMSource>(identifier.UTF8String,
+    auto ident = std::string(identifier.UTF8String);
+    auto source = std::make_unique<mbgl::style::RasterDEMSource>(ident,
                                                                  urlOrTileset,
                                                                  tileSize);
     return source;
